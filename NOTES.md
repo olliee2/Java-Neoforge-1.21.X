56127200005845c4a5aac6b207371f501d4a812c
olliee2 <218727432+olliee2@users.noreply.github.com>
2025-07-03 17:09:23 +0100

    Add crafting recipes for Blazing Kricketot and Kricketotium Block

---
+++ NOTES.md
@@ -0,0 +1,7 @@
+
+
+data is running.
+client is running.
+game is opening.
+ok this tab is now pinned.
+always write in here if there's something I'm thinking about.
--- src/main/java/net/olliee2/kricketotmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/olliee2/kricketotmod/datagen/ModRecipeProvider.java
@@ -1,17 +1,18 @@
 package net.olliee2.kricketotmod.datagen;
 
-import net.neoforged.neoforge.common.conditions.IConditionBuilder;
-import net.olliee2.kricketotmod.KricketotMod;
-import net.olliee2.kricketotmod.block.ModBlocks;
-import net.olliee2.kricketotmod.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.*;
 import net.minecraft.world.level.ItemLike;
+import net.neoforged.neoforge.common.conditions.IConditionBuilder;
+import net.olliee2.kricketotmod.KricketotMod;
+import net.olliee2.kricketotmod.block.ModBlocks;
+import net.olliee2.kricketotmod.item.ModItems;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -23,25 +24,6 @@ import static net.minecraft.data.recipes.ShapelessRecipeBuilder.shapeless;
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
     public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries);
-    }
-
-    protected void buildRecipes(@NotNull RecipeOutput recipeOutput) {
-        List<ItemLike> KRICKETOTIUM_SMELTABLES = List.of(ModItems.RAW_KRICKETOTIUM,
-                ModBlocks.KRICKETOTIUM_ORE, ModBlocks.DEEPSLATE_KRICKETOTIUM_ORE);
-
-        shaped(RecipeCategory.MISC, ModBlocks.KRICKETOTIUM_BLOCK.get())
-                .pattern("BBB")
-                .pattern("BBB")
-                .pattern("BBB")
-                .define('B', ModItems.KRICKETOTIUM.get())
-                .unlockedBy("has_kricketotium", has(ModItems.KRICKETOTIUM)).save(recipeOutput);
-
-        shapeless(RecipeCategory.MISC, ModItems.KRICKETOTIUM.get(), 9)
-                .requires(ModBlocks.KRICKETOTIUM_BLOCK)
-                .unlockedBy("has_kricketotium_block", has(ModBlocks.KRICKETOTIUM_BLOCK)).save(recipeOutput);
-
-        oreSmelting(recipeOutput, KRICKETOTIUM_SMELTABLES, RecipeCategory.MISC, ModItems.KRICKETOTIUM.get(), 0.25f, 200, "kricketotium");
-        oreBlasting(recipeOutput, KRICKETOTIUM_SMELTABLES, RecipeCategory.MISC, ModItems.KRICKETOTIUM.get(), 0.25f, 100, "kricketotium");
     }
 
     protected static void oreSmelting(@NotNull RecipeOutput recipeOutput, List<ItemLike> pIngredients, @NotNull RecipeCategory pCategory, @NotNull ItemLike pResult,
@@ -58,9 +40,36 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
 
     protected static <T extends AbstractCookingRecipe> void oreCooking(@NotNull RecipeOutput recipeOutput, RecipeSerializer<T> pCookingSerializer, AbstractCookingRecipe.@NotNull Factory<T> factory,
                                                                        List<ItemLike> pIngredients, @NotNull RecipeCategory pCategory, @NotNull ItemLike pResult, float pExperience, int pCookingTime, @NotNull String pGroup, String pRecipeName) {
-        for(ItemLike itemlike : pIngredients) {
+        for (ItemLike itemlike : pIngredients) {
             SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), pCategory, pResult, pExperience, pCookingTime, pCookingSerializer, factory).group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
                     .save(recipeOutput, KricketotMod.MOD_ID + ":" + getItemName(pResult) + pRecipeName + "_" + getItemName(itemlike));
         }
+    }
+
+    protected void buildRecipes(@NotNull RecipeOutput recipeOutput) {
+        List<ItemLike> KRICKETOTIUM_SMELTABLES = List.of(ModItems.RAW_KRICKETOTIUM,
+                ModBlocks.KRICKETOTIUM_ORE, ModBlocks.DEEPSLATE_KRICKETOTIUM_ORE);
+
+        shaped(RecipeCategory.MISC, ModBlocks.KRICKETOTIUM_BLOCK.get())
+                .pattern("KKK")
+                .pattern("KKK")
+                .pattern("KKK")
+                .define('K', ModItems.KRICKETOTIUM.get())
+                .unlockedBy("has_kricketotium", has(ModItems.KRICKETOTIUM.get())).save(recipeOutput);
+
+        shaped(RecipeCategory.MISC, ModItems.BLAZING_KRICKETOT.get(), 8)
+                .pattern("KKK")
+                .pattern("KBK")
+                .pattern("KKK")
+                .define('K', ModItems.KRICKETOTIUM.get())
+                .define('B', Items.BLAZE_POWDER)
+                .unlockedBy("has_kricketotium", has(ModItems.KRICKETOTIUM.get())).save(recipeOutput);
+
+        shapeless(RecipeCategory.MISC, ModItems.KRICKETOTIUM.get(), 9)
+                .requires(ModBlocks.KRICKETOTIUM_BLOCK)
+                .unlockedBy("has_kricketotium_block", has(ModBlocks.KRICKETOTIUM_BLOCK)).save(recipeOutput);
+
+        oreSmelting(recipeOutput, KRICKETOTIUM_SMELTABLES, RecipeCategory.MISC, ModItems.KRICKETOTIUM.get(), 0.25f, 200, "kricketotium");
+        oreBlasting(recipeOutput, KRICKETOTIUM_SMELTABLES, RecipeCategory.MISC, ModItems.KRICKETOTIUM.get(), 0.25f, 100, "kricketotium");
     }
 }
